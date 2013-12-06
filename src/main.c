bd279928c2e99f55596483c727b386f87640ef2f
Daniel Burke <dan.p.burke@gmail.com>
2013-12-06 20:49:49 +1030

    Many ray errors fixed

--- src/main.c
+++ src/main.c
@@ -158,7 +158,7 @@ void main_loop(void)
 
 	time = (float)(sys_time() - time_start)/(float)sys_ticksecond;
 
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
 	gui_input();
